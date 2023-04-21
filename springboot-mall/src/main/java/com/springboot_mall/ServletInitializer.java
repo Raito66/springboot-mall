897af49daa4569a959842ff27564ed0e60da7d89
HYL <tfy4942@gmail.com>
2023-04-21 18:22:10 +0800

    商品功能 - 實作修改商品

--- springboot-mall/src/main/java/com/springboot_mall/ServletInitializer.java
+++ springboot-mall/src/main/java/com/springboot_mall/ServletInitializer.java
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(StayhomeApplication.class);
+		return application.sources(SpringbootMallApplication.class);
 	}
 
 }
