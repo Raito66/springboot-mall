1e45d45c312876fc0fa9535ce9119a70c0c98c27
HYL <tfy4942@gmail.com>
2023-05-05 14:25:29 +0800

    商品功能 - 實作查詢商品列表(排序)

--- springboot-mall/src/main/java/com/springboot_mall/dto/ProductQueryParams.java
+++ springboot-mall/src/main/java/com/springboot_mall/dto/ProductQueryParams.java
@@ -5,6 +5,8 @@ import com.springboot_mall.constant.ProductCategory;
 public class ProductQueryParams {
 	private ProductCategory category;
 	private String search;
+	private String orderBy;
+	private String sort;
 	
 	public ProductCategory getCategory() {
 		return category;
@@ -19,5 +21,17 @@ public class ProductQueryParams {
 		this.search = search;
 	}
 	
+	public String getOrderBy() {
+		return orderBy;
+	}
+	public void setOrderBy(String orderBy) {
+		this.orderBy = orderBy;
+	}
+	public String getSort() {
+		return sort;
+	}
+	public void setSort(String sort) {
+		this.sort = sort;
+	}
 	
 }
